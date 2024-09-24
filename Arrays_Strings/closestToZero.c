f4f303fa41a642cf09dab2dc9d2cfe84450a59be
Nathan Gabriel <126394048+NathanGbl@users.noreply.github.com>
2024-09-24 20:04:17 +0000

    Continuação do exercício.

--- Arrays_Strings/closestToZero.c
+++ Arrays_Strings/closestToZero.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
 int findClosestNumber(int* nums, int numSize) {
-    signed int distanceFromZero;
-    signed int numNow = nums[0];
-    if (numNow < 0) {
-        distanceFromZero = numNow * -1;
+    int distanceFromZero;
+    if (nums[0] < 0) {
+        distanceFromZero = nums[0] * -1;
     } else {
-        distanceFromZero = numNow;
+        distanceFromZero = nums[0];
     }
     for (int i = 1; i < numSize; i++) {
-        if (nums[i] == 0 || numNow == 0) {
+        if (nums[i] == 0 || distanceFromZero == 0) {
             return 0;
         }
-        if (numNow < 0) {
-            if (nums[i] > 0 && distanceFromZero > nums[i]) {
-                distanceFromZero = nums[i];
-                numNow = nums[i];
-            } else if (nums[i] < 0 && nums[i] > numNow && distanceFromZero > nums[i] * -1) {
-                distanceFromZero = nums[i] * -1;
-                numNow = nums[i];
-            }
-        } else if (numNow > 0) {
-            
+        if (nums[i] > 0 && distanceFromZero > nums[i]) {
+            distanceFromZero = nums[i];
+        } else if (nums[i] < 0 && distanceFromZero > nums[i] * -1){
+            distanceFromZero = nums[i] * -1;
         }
     }
+    return distanceFromZero;
 }
